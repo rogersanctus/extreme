f9b9ef11bc0ad85eeec40f7a3b1dd2bdb3f48f99
rogersanctus <rogersanctus@gmail.com>
2023-04-24 18:27:34 -0300

    feat: removed the Protobuf macro for modules generation

--- lib/extreme/messages.ex
+++ lib/extreme/messages.ex
@@ -1,10 +1,7 @@
 defmodule Extreme.Messages do
   @moduledoc """
-  Messages are based on  official proto file: 
+  Messages are based on  official proto file:
   https://github.com/EventStore/EventStore/blob/master/src/Protos/ClientAPI/ClientMessageDtos.proto
   + ReadStreamEventsBackward message (that works but is missing in their proto file)
   """
-  use Protobuf,
-    from: Path.expand("../../include/event_store.proto", __DIR__),
-    use_package_names: true
 end
